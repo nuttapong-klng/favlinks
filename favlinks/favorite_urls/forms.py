fc587a653f7a7378c3ba3e6b8d3a7c5a6735560f
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2024-10-19 21:39:49 +0700

    Add category and tag

--- favlinks/favorite_urls/forms.py
+++ favlinks/favorite_urls/forms.py
@@ -1,14 +1,20 @@
 from django import forms
 
+from .models import Category
 from .models import FavoriteUrl
+from .models import Tag
 
 
 class FavoriteUrlForm(forms.ModelForm):
     title = forms.CharField()
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all())
 
     class Meta:
         model = FavoriteUrl
         fields = [
             "title",
             "url",
+            "category",
+            "tags",
         ]
--- favlinks/favorite_urls/models.py
+++ favlinks/favorite_urls/models.py
@@ -9,8 +9,34 @@ class FavoriteUrl(models.Model):
         on_delete=models.CASCADE,
         related_name="favorite_urls",
     )
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.CASCADE,
+        related_name="favorite_urls",
+        blank=True,
+        null=True,
+    )
+    tags = models.ManyToManyField(
+        "Tag",
+        related_name="favorite_urls",
+        blank=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
