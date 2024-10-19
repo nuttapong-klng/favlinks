ca0bb20df1fb84e87584fca4aa1b853d743dd110
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2024-10-19 11:56:06 +0700

    Formatting code

--- favlinks/favorite_urls/models.py
+++ favlinks/favorite_urls/models.py
@@ -5,7 +5,9 @@ class FavoriteUrl(models.Model):
     url = models.URLField()
     title = models.TextField()
     owner = models.ForeignKey(
-        "users.User", on_delete=models.CASCADE, related_name="favorite_urls"
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="favorite_urls",
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
