cd72f7add9d6cc3e18b3fe1a85e2e02941b1c492
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2024-10-19 15:03:40 +0700

    Add list view

--- favlinks/favorite_urls/urls.py
+++ favlinks/favorite_urls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import favorite_url_create_view
+from .views import favorite_url_list_view
 
 app_name = "favorite_urls"
 urlpatterns = [
     path("create/", view=favorite_url_create_view, name="create"),
+    path("list/", view=favorite_url_list_view, name="list"),
 ]
--- favlinks/favorite_urls/views.py
+++ favlinks/favorite_urls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views import View
 
 from favlinks.favorite_urls.forms import FavoriteUrlForm
+from favlinks.favorite_urls.models import FavoriteUrl
 
 
 class FavoriteUrlCreateView(LoginRequiredMixin, View):
@@ -17,8 +18,15 @@ class FavoriteUrlCreateView(LoginRequiredMixin, View):
         form.instance.owner = request.user
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return redirect("favorite_urls:list")
         return None
 
 
+class FavoriteUrlListView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        context = {"favorite_urls": FavoriteUrl.objects.filter(owner=request.user)}
+        return render(request, "favorite_urls/favoriteurl_list.html", context)
+
+
 favorite_url_create_view = FavoriteUrlCreateView.as_view()
+favorite_url_list_view = FavoriteUrlListView.as_view()
