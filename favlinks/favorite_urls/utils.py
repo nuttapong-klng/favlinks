0661271fac4fbe0dd064566f8437be75d6586d7d
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2024-10-19 21:11:59 +0700

    Add get and validate ownership of FavoriteUrl function

---
+++ favlinks/favorite_urls/utils.py
@@ -0,0 +1,25 @@
+from django.views import defaults as default_views
+
+from favlinks.favorite_urls.models import FavoriteUrl
+
+
+def get_and_validate_ownership_of_favorite_url(request, favorite_url_id):
+    error = False
+    error_view = None
+    favorite_url = FavoriteUrl.objects.filter(
+        id=favorite_url_id,
+        owner=request.user,
+    ).first()
+    if not favorite_url:
+        error = True
+        error_view = default_views.page_not_found(
+            request,
+            Exception("Favorite URL not found"),
+        )
+    if favorite_url.owner != request.user:
+        error = True
+        error_view = default_views.permission_denied(
+            request,
+            Exception("You are not the owner of this favorite URL"),
+        )
+    return favorite_url, error, error_view
