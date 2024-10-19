60a4b33aa9f56547bebe4864193919ac02a6a99d
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2024-10-19 15:26:49 +0700

    Add edit view

--- favlinks/favorite_urls/urls.py
+++ favlinks/favorite_urls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import favorite_url_create_view
+from .views import favorite_url_edit_view
 from .views import favorite_url_list_view
 
 app_name = "favorite_urls"
 urlpatterns = [
     path("create/", view=favorite_url_create_view, name="create"),
+    path("edit/<int:id>/", view=favorite_url_edit_view, name="edit"),
     path("list/", view=favorite_url_list_view, name="list"),
 ]
--- favlinks/favorite_urls/views.py
+++ favlinks/favorite_urls/views.py
@@ -10,12 +10,34 @@ from favlinks.favorite_urls.models import FavoriteUrl
 class FavoriteUrlCreateView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         form = FavoriteUrlForm()
-        context = {"form": form}
+        context = {
+            "form": form,
+            "action": "Create",
+        }
         return render(request, "favorite_urls/favoriteurl_create.html", context)
 
     def post(self, request, *args, **kwargs):
         form = FavoriteUrlForm(request.POST)
         form.instance.owner = request.user
+        if form.is_valid():
+            form.save()
+            return redirect("favorite_urls:list")
+        return None
+
+
+class FavoriteUrlEditView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        favorite_url = FavoriteUrl.objects.get(id=kwargs["id"], owner=request.user)
+        form = FavoriteUrlForm(instance=favorite_url)
+        context = {
+            "form": form,
+            "action": "Edit",
+        }
+        return render(request, "favorite_urls/favoriteurl_create.html", context)
+
+    def post(self, request, *args, **kwargs):
+        favorite_url = FavoriteUrl.objects.get(id=kwargs["id"], owner=request.user)
+        form = FavoriteUrlForm(request.POST, instance=favorite_url)
         if form.is_valid():
             form.save()
             return redirect("favorite_urls:list")
@@ -29,4 +51,5 @@ class FavoriteUrlListView(LoginRequiredMixin, View):
 
 
 favorite_url_create_view = FavoriteUrlCreateView.as_view()
+favorite_url_edit_view = FavoriteUrlEditView.as_view()
 favorite_url_list_view = FavoriteUrlListView.as_view()
